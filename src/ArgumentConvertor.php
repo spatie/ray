1f3dca5cbcb8a7eb58f65da6d73e683a535d3f4b
freek <freek@spatie.be>
2020-12-17 21:15:06 +0100

    wip

---
+++ src/ArgumentConvertor.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Spatie\Ray;
+
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+
+class ArgumentConvertor
+{
+    public static function convertToPrimitive($argument)
+    {
+        if (is_string($argument)) {
+            return $argument;
+        }
+
+        if (is_int($argument)) {
+            return $argument;
+        }
+
+        if (is_bool($argument)) {
+            return $argument;
+        }
+
+        $cloner = new VarCloner();
+
+        $dumper = new HtmlDumper();
+
+        $clonedArgument = $cloner->cloneVar($argument);
+
+        return $dumper->dump($clonedArgument, true);
+    }
+}
--- src/Payloads/LogPayload.php
+++ src/Payloads/LogPayload.php
@@ -3,44 +3,21 @@
 namespace Spatie\Ray\Payloads;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Ray\ArgumentConvertor;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 
 class LogPayload extends Payload
 {
-    /** @var mixed */
     protected array $values;
 
     public static function createForArguments(array $arguments): Payload
     {
         $dumpedArguments = array_map(function ($argument) {
-            return self::convertToPrimitive($argument);
+            return ArgumentConvertor::convertToPrimitive($argument);
         }, $arguments);
 
         return new static($dumpedArguments);
-    }
-
-    protected static function convertToPrimitive($argument)
-    {
-        if (is_string($argument)) {
-            return $argument;
-        }
-
-        if (is_int($argument)) {
-            return $argument;
-        }
-
-        if (is_bool($argument)) {
-            return $argument;
-        }
-
-        $cloner = new VarCloner();
-
-        $dumper = new HtmlDumper();
-
-        $clonedArgument = $cloner->cloneVar($argument);
-
-        return $dumper->dump($clonedArgument, true);
     }
 
     public function __construct($values)
