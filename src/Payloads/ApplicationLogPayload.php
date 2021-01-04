70fada581d7720be4aba59bb9ba20f72ff6aa3f5
freek <freek@spatie.be>
2021-01-04 17:24:58 +0100

    wip

---
+++ src/Payloads/ApplicationLogPayload.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Spatie\Ray\Payloads;
+
+class ApplicationLogPayload extends Payload
+{
+    protected string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function getType(): string
+    {
+        return 'application_log';
+    }
+
+    public function getContent(): array
+    {
+        return [
+            'value' => $this->value,
+        ];
+    }
+}
