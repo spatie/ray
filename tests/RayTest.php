4a780c4a44ab7ecc048357038d4aaa1916e5b8dc
freek <freek@spatie.be>
2021-01-07 19:26:13 +0100

    wip

--- tests/RayTest.php
+++ tests/RayTest.php
@@ -53,7 +53,10 @@ class RayTest extends TestCase
     {
         $this->ray->send(['a' => 1, 'b' => 2]);
 
-        $this->assertMatchesSnapshot($this->client->sentPayloads());
+        $dumpedValue = $this->getValueOfLastSentContent('values')[0];
+
+        $this->assertStringContainsString('<span class=sf-dump-key>a</span>', $dumpedValue);
+        $this->assertStringContainsString('<span class=sf-dump-key>b</span>', $dumpedValue);
     }
 
     /** @test */
@@ -107,11 +110,11 @@ class RayTest extends TestCase
     /** @test */
     public function it_can_conditionally_show_something_using_a_callable()
     {
-        $this->ray->send('hey')->showIf(fn () => true);
+        $this->ray->send('hey')->showIf(fn() => true);
         $this->assertCount(1, $this->client->sentPayloads());
 
         $this->client->reset();
-        $this->ray->send('hey')->showIf(fn () => false);
+        $this->ray->send('hey')->showIf(fn() => false);
         $this->assertCount(2, $this->client->sentPayloads());
     }
 
@@ -129,11 +132,11 @@ class RayTest extends TestCase
     /** @test */
     public function it_can_conditionally_remove_something_using_a_callable()
     {
-        $this->ray->send('hey')->removeWhen(fn () => true);
+        $this->ray->send('hey')->removeWhen(fn() => true);
         $this->assertCount(2, $this->client->sentPayloads());
 
         $this->client->reset();
-        $this->ray->send('hey')->removeWhen(fn () => false);
+        $this->ray->send('hey')->removeWhen(fn() => false);
         $this->assertCount(1, $this->client->sentPayloads());
     }
 
@@ -216,7 +219,7 @@ class RayTest extends TestCase
     /** @test */
     public function it_can_send_backtrace_frames_starting_from_a_specific_frame()
     {
-        $this->ray->trace(fn (Frame $frame) => $frame->class === 'PHPUnit\TextUI\TestRunner');
+        $this->ray->trace(fn(Frame $frame) => $frame->class === 'PHPUnit\TextUI\TestRunner');
 
         $frames = $this->getValueOfLastSentContent('frames');
 
@@ -321,7 +324,7 @@ class RayTest extends TestCase
     {
         $payload = $this->client->sentPayloads();
 
-        if (! count($payload)) {
+        if (!count($payload)) {
             return null;
         }
 
