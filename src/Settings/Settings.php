80bd1cf28f68b8dd8e11958b9e9d1b21f12cecab
freekmurze <freekmurze@users.noreply.github.com>
2021-02-26 11:02:34 +0000

    Fix styling

--- src/Settings/Settings.php
+++ src/Settings/Settings.php
@@ -34,7 +34,7 @@ class Settings
 
     public function setDefaultSettings(array $defaults)
     {
-        foreach($defaults as $name => $value) {
+        foreach ($defaults as $name => $value) {
             if ($this->shouldSetSetting($name)) {
                 $this->settings[$name] = $value;
             }
