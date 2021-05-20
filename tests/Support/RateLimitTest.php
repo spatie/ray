0ff492edee6ce58f984b63f5612cf9d105632901
Niels <niels@spatie.be>
2021-05-20 09:07:32 +0200

    coverage for the immutable getters and setters

--- tests/Support/RateLimitTest.php
+++ tests/Support/RateLimitTest.php
@@ -15,4 +15,28 @@ class RateLimitTest extends TestCase
         $this->assertNull($rateLimit->getMax());
         $this->assertNull($rateLimit->getPerSecond());
     }
+
+    /** @test */
+    public function it_can_update_the_max_calls(): void
+    {
+        $rateLimit = RateLimit::disabled();
+
+        $newRateLimit = $rateLimit->max(25);
+
+        $this->assertNull($rateLimit->getMax()); // immutable check
+
+        $this->assertSame(25, $newRateLimit->getMax());
+    }
+
+    /** @test */
+    public function it_can_update_the_per_second_calls(): void
+    {
+        $rateLimit = RateLimit::disabled();
+
+        $newRateLimit = $rateLimit->perSecond(25);
+
+        $this->assertNull($rateLimit->getPerSecond()); // immutable check
+
+        $this->assertSame(25, $newRateLimit->getPerSecond());
+    }
 }
