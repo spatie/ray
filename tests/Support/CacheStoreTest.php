a7a9140381d36071bb924e696898e1c4302be541
freekmurze <freekmurze@users.noreply.github.com>
2021-05-31 17:15:53 +0000

    Fix styling

--- tests/Support/CacheStoreTest.php
+++ tests/Support/CacheStoreTest.php
@@ -5,7 +5,6 @@ namespace Spatie\Ray\Tests\Support;
 use PHPUnit\Framework\TestCase;
 use Spatie\Ray\Support\CacheStore;
 use Spatie\Ray\Support\Clock;
-use Spatie\Ray\Tests\TestClasses\FakeClock;
 
 class CacheStoreTest extends TestCase
 {
