528a98cae6f76d654dd5f462f063b7f7967e625a
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-14 21:40:05 +0300

    refactor PayloadFactoryTest

--- tests/PayloadFactoryTest.php
+++ tests/PayloadFactoryTest.php
@@ -1,24 +1,17 @@
 <?php
 
-namespace Spatie\Ray\Tests;
-
 use Carbon\Carbon;
 use Carbon\CarbonImmutable;
-use PHPUnit\Framework\TestCase;
 use Spatie\Ray\PayloadFactory;
 use Spatie\Ray\Payloads\CarbonPayload;
+use function PHPUnit\Framework\assertContainsOnlyInstancesOf;
 
-class PayloadFactoryTest extends TestCase
-{
-    /** @test */
-    public function it_accepts_carboninterface_instances_as_carbonpayload_argument(): void
-    {
-        /** @var CarbonPayload[] $payloads */
-        $payloads = (new PayloadFactory([
-            Carbon::now(),
-            CarbonImmutable::now(),
-        ]))->getPayloads();
+it('accepts carboninterface instances as carbonpayload argument', function () {
+    /** @var CarbonPayload[] $payloads */
+    $payloads = (new PayloadFactory([
+        Carbon::now(),
+        CarbonImmutable::now(),
+    ]))->getPayloads();
 
-        self::assertContainsOnlyInstancesOf(CarbonPayload::class, $payloads);
-    }
-}
+    assertContainsOnlyInstancesOf(CarbonPayload::class, $payloads);
+});
